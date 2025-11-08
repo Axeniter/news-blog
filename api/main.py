1c8ded91458454330b9f05d3c49bc1776ae47ebc
Axeniter <anamievoid@gmail.com>
2025-11-08 20:21:50 +1000

    Add: main fastapi app
    add main.py with fastapi app initialization
    add run.py with uvicorn server start
    rename folder blog to api

---
+++ api/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
+from fastapi.responses import HTMLResponse
+from auth_endpoints import auth_router
+from comment_endpoints import comment_router
+from article_endpoints import article_router
+from database import create_tables
+
+app = FastAPI()
+create_tables()
+
+app.include_router(article_router)
+app.include_router(comment_router)
+app.include_router(auth_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+    allow_origins=["http://localhost:3000"],
+)
+ 
+@app.get("/")
+def get_root():
+    html_content = "<h2>API включено</h2>"
+    return HTMLResponse(content=html_content)
